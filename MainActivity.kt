0f7403ef0330a7425252951cdba27652c8d67e35
Alexander1909 <jhonathan.corrales@ucsm.edu.pe>
2026-03-27 08:26:00 -0500

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -17,3 +17,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+         etNombre = findViewById(R.id.etNombre)
+        etApellidos = findViewById(R.id.etApellidos)
+        etEdad = findViewById(R.id.etEdad)
+        btnRegistrar = findViewById(R.id.btnRegistrar)
+        tvResultado = findViewById(R.id.tvResultado)
+
+        btnRegistrar.setOnClickListener {
+
+            val nombre = etNombre.text.toString()
+            val apellidos = etApellidos.text.toString()
+            val edad = etEdad.text.toString()
+
+            if (nombre.isEmpty() || apellidos.isEmpty() || edad.isEmpty()) {
+                tvResultado.text = "Por favor complete todos los campos"
+            } else {
+                tvResultado.text = """
+                    Estudiante registrado:
+                    Nombre: $nombre
+                    Apellidos: $apellidos
+                    Edad: $edad
+                """.trimIndent()
+            }
+        }
+    }
+}
