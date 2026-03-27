e2ea83936acffafb989e325bf41eaaad0b8df53d
Alexander1909 <jhonathan.corrales@ucsm.edu.pe>
2026-03-27 08:25:46 -0500

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -6,16 +6,14 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etNombre: EditText
+    private lateinit var etApellidos: EditText
+    private lateinit var etEdad: EditText
+    private lateinit var btnRegistrar: Button
+    private lateinit var tvResultado: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
-}
