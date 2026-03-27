be8287796a0ba0b67f7d437da321a5cc38a5a835
Alexander1909 <jhonathan.corrales@ucsm.edu.pe>
2026-03-27 08:25:30 -0500

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.registrodeestudiante
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
